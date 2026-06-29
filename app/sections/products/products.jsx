fbf090db206a6206e0bfe0e74d81283cdbe10222
Murtaza Siddiqui <murtazasiddiqui250@gmail.com>
2026-06-29 12:56:36 +0500

    Update featured products in ProductSection component

--- app/sections/products/products.jsx
+++ app/sections/products/products.jsx
@@ -1,10 +1,10 @@
 import './products.css'
 const ProductSection = () => { 
     const mockFeaturedProducts = [
-    { id: "p1", name: "Heavyweight Core Tee", price: "$29.00", img: "https://images.unsplash.com/photo-1521572267360-ee0c2909d518?q=80&w=500" },
-    { id: "p2", name: "Luxury Oversized Hoodie", price: "$65.00", img: "https://images.unsplash.com/photo-1556821840-3a63f95609a7?q=80&w=500" },
-    { id: "p3", name: "Classic French Terry Crewneck", price: "$48.00", img: "https://images.unsplash.com/photo-1620799140408-edc6dcb6d633?q=80&w=500" },
-    { id: "p4", name: "Custom DTF Workwear Jacket", price: "$89.00", img: "https://images.unsplash.com/photo-1551028719-00167b16eac5?q=80&w=500" },
+    { id: "p1", name: "E2-001", price: "$29.00", img: "https://i.pinimg.com/736x/f7/b5/4a/f7b54ac80ad7812b34b90c3d0f227c09.jpg" },
+    { id: "p2", name: "E3-054", price: "$65.00", img: "https://i.pinimg.com/736x/33/5f/98/335f987712c0a02e277ad02ce1536cca.jpg" },
+    { id: "p3", name: "ES-004", price: "$48.00", img: "https://i.pinimg.com/736x/97/c1/54/97c154119f4563861e9067d8cd08d637.jpg" },
+    { id: "p4", name: "EL-035", price: "$89.00", img: "https://i.pinimg.com/736x/6d/e5/cc/6de5cc4189fe747caf09eaabf5aa0a13.jpg" },
   ];
 
   return(
