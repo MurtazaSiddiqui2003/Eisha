d9b03587edae296f2ccd430ef18143c6ec841541
Murtaza Siddiqui <murtazasiddiqui250@gmail.com>
2026-06-29 13:19:19 +0500

    Revise product details in ProductDetailPage
    Updated product inventory and descriptions in the ProductDetailPage component.

--- app/products/[id]/page.jsx
+++ app/products/[id]/page.jsx
@@ -15,10 +15,12 @@ const ProductDetailPage = ({ params: paramsPromise }) => {
 
   // Mock catalog dataset (In Phase 5, we fetch this directly from MongoDB using this ID!)
   const mockInventory = {
-    p1: { name: "Heavyweight Core Tee", price: "$29.00", img: "https://images.unsplash.com/photo-1521572267360-ee0c2909d518?q=80&w=500", desc: "A robust 240GSM cotton luxury blank built with drop shoulders and tight collar ribbing. Optimized perfectly for direct structural garment treatments." },
-    p2: { name: "Luxury Oversized Hoodie", price: "$65.00", img: "https://images.unsplash.com/photo-1556821840-3a63f95609a7?q=80&w=500", desc: "450GSM French Terry weave featuring double-lined structural hoods without drawstrings. The perfect clean canvas for heavy graphical DTF impressions." },
-    p3: { name: "Classic French Terry Crewneck", price: "$48.00", img: "https://images.unsplash.com/photo-1620799140408-edc6dcb6d633?q=80&w=500", desc: "Tailored comfortable loungewear built from tightly spun yarn loops. Keeps its color grade clean and sharp across infinite wash cycles." },
-    p4: { name: "Custom DTF Workwear Jacket", price: "$89.00", img: "https://images.unsplash.com/photo-1551028719-00167b16eac5?q=80&w=500", desc: "Durable duck canvas outerwear treated with industrial windbreakers. Outfitted with massive surface backpanels for high-impact brand art prints." }
+    p1: { name: "E2-001", price: "$29.00", img: "https://i.pinimg.com/736x/f7/b5/4a/f7b54ac80ad7812b34b90c3d0f227c09.jpg", desc: "A Traditional 2 Pieces suit Including Kurta, Pajama." },
+    p2: { name: "E3-054", price: "$65.00", img: "https://i.pinimg.com/736x/33/5f/98/335f987712c0a02e277ad02ce1536cca.jpg", desc: "A Traditional 3 Pieces Suit Including Kurta, Pajama, Dupatta." },
+    p3: { name: "ES-004", price: "$48.00", img: "https://i.pinimg.com/736x/97/c1/54/97c154119f4563861e9067d8cd08d637.jpg", desc: "An Elegant Traditional Saree For Special Occasions" },
+    p4: { name: "EL-035", price: "$89.00", img: "https://i.pinimg.com/736x/6d/e5/cc/6de5cc4189fe747caf09eaabf5aa0a13.jpg", desc: "A Special Lehnga For Weddings" },
+    p5: { name: "E3-021", price: "$24.00", img: "https://i.pinimg.com/736x/f6/3b/c5/f63bc520c22b165af8120bf8cba20f3c.jpg", desc: "A Traditional 2 Pieces suit Including Kurta, Pajama." },
+    p6: { name: "E2-019", price: "$35.00", img: "https://i.pinimg.com/736x/c1/8e/69/c18e69c32510f6449431a1fcb0a6f09b.jpg", desc: "A Traditional 3 Pieces Suit Including Kurta, Pajama, Dupatta." },
   };
 
   // Find product or fall back safely
