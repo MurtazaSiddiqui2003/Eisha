d008525b9b509fffe5c6d1dd689a084f31a0aac4
Murtaza Siddiqui <murtazasiddiqui250@gmail.com>
2026-06-29 12:28:49 +0500

    Update collection items in CollectionSection component

--- app/sections/collections/collection.jsx
+++ app/sections/collections/collection.jsx
@@ -3,10 +3,10 @@ import './collecton.css'
 const CollectionSection = () => {
 
     const mockCollection = [
-        { id: 1, name: "Premium Blanks", bg: "https://images.unsplash.com/photo-1521572267360-ee0c2909d518?q=80&w=500" },
-        { id: 2, name: "Custom Hoodies", bg: "https://images.unsplash.com/photo-1556821840-3a63f95609a7?q=80&w=500" },
-        { id: 3, name: "Print-on-Demand", bg: "https://images.unsplash.com/photo-1618220179428-22790b461013?q=80&w=500" },
-        { id: 4, name: "Embroidery Services", bg: "https://images.unsplash.com/photo-1617038260897-41a1f14a8ca0?q=80&w=500" },
+        { id: 1, name: "2 Piece Suits", bg: "https://i.pinimg.com/1200x/93/c2/ff/93c2fff7f0cad64d25856c2f577f2cf5.jpg" },
+        { id: 2, name: "3 Piece Suits", bg: "https://i.pinimg.com/736x/86/fa/53/86fa53ef2ada3d72dc932a3cc972ab66.jpg" },
+        { id: 3, name: "Saree", bg: "https://i.pinimg.com/1200x/11/91/be/1191be877ceb3e7e6ae566a731a90656.jpg" },
+        { id: 4, name: "Lehnga", bg: "https://i.pinimg.com/1200x/e6/20/72/e620729108c072fddbcae017f23ff4bc.jpg" },
     ]
 
     return(
