a423b289e084e576ff5e67e0ddae01d99cf39bdd
Murtaza Siddiqui <murtazasiddiqui250@gmail.com>
2026-07-01 02:43:04 +0500

    Add files via upload

--- app/sections/hero/hero.jsx
+++ app/sections/hero/hero.jsx
@@ -5,7 +5,7 @@ import heroImage from '../../assets/img/hero16.jpeg'
 const Hero = () => {
     return(
         <section className="hero-section">
-            <Image src={heroImage} alt="Hero Image" Width='100%' Hzzeeight='auto' />
+            <Image src={heroImage} alt="Hero Image" Width='100%' Height='auto' />
         </section>
     )
 }
