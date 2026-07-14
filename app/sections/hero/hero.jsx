bde7279659b988c0bdb619ef177d813ccc117d36
Murtaza Siddiqui <murtazasiddiqui250@gmail.com>
2026-07-14 12:27:24 +0500

    Refactor Hero component to include CSS styling
    Added CSS import for styling the hero section.

--- app/sections/hero/hero.jsx
+++ app/sections/hero/hero.jsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 
+import './hero.css'
+
 import heroImage from '../../assets/img/hero16.jpeg'
 
 const Hero = () => {
     return(
         <section className="hero-section">
-            <Image src={heroImage} alt="Hero Image" Width='100vw' Height='auto' />
+            <Image src={heroImage} alt="Hero Image" Width='100%' Height='auto' />
         </section>
     )
 }
